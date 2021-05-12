b2704521d4ee0054dbf550e469b9f418854c48d4
nikimanoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2021-05-12 16:20:24 +0100

    Add Nodegroup_UpdateConfig struct to EKS

--- cloudformation/eks/aws-eks-nodegroup.go
+++ cloudformation/eks/aws-eks-nodegroup.go
@@ -94,6 +94,11 @@ type Nodegroup struct {
 	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-eks-nodegroup.html#cfn-eks-nodegroup-taints
 	Taints []*Nodegroup_Taints `json:"Taints,omitempty"`
 
+	// UpdateConfig AWS CloudFormation Property
+	// Required: false
+	// TODO @nikimanoledaki: Add link to docs
+	UpdateConfig *Nodegroup_UpdateConfig `json:"UpdateConfig,omitempty"`
+
 	// Version AWS CloudFormation Property
 	// Required: false
 	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-eks-nodegroup.html#cfn-eks-nodegroup-version
