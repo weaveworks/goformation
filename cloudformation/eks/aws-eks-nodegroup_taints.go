22fe45fb51fd4babfaf69f32bc60f40fa259e73c
Chetan Patwal <cPu1@users.noreply.github.com>
2021-05-12 13:11:28 +0530

    Update cloudformation/eks/aws-eks-nodegroup_taints.go
    Co-authored-by: Jake Klein <jake@weave.works>

--- cloudformation/eks/aws-eks-nodegroup_taints.go
+++ cloudformation/eks/aws-eks-nodegroup_taints.go
@@ -3,7 +3,7 @@ package eks
 import "github.com/weaveworks/goformation/v4/cloudformation/types"
 
 // Nodegroup_Taints AWS CloudFormation Resource (AWS::EKS::Nodegroup.Taints)
-// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-eks-nodegroup-taints.html
+// See: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-eks-nodegroup-taint.html
 type Nodegroup_Taints struct {
 	// TODO documentation
 	Key    *types.Value `json:"Key,omitempty"`
